43b3e7d0e57b7823be143b56e24a2d97169b5c75
KristiSeraj <serajkristi@gmail.com>
2025-01-27 14:37:21 +0100

    New components and routes added

---
+++ resources/views/product.blade.php
@@ -0,0 +1,26 @@
+<x-layout>
+    <x-slot:heading>
+        {{ $product['title'] }}
+    </x-slot:heading>
+    <div class="bg-white">
+        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+            <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                <div class="group relative">
+                    <img src="https://i.pinimg.com/236x/8f/0f/3e/8f0f3ed4c2ba9e3094278d31c699b9d1.jpg" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="aspect-square w-full rounded-lg bg-gray-200 object-cover group-hover:opacity-75 xl:aspect-7/8">
+                    <div class="mt-4 flex justify-between">
+                        <div>
+                            <h3 class="text-sm text-gray-700">
+                                <a href="#">
+                                    <span aria-hidden="true" class="absolute inset-0"></span>
+                                    {{ $product['title'] }}
+                                </a>
+                            </h3>
+                            <p class="mt-1 text-sm text-gray-500">{{ $product['style'] }}</p>
+                        </div>
+                        <p class="text-sm font-medium text-gray-900">{{ $product['price'] }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,68 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'greetings' => 'Hello',
+    ]);
 });
 
 Route::get('/about', function() {
     return view('about');
+});
+
+Route::get('/products', function() {
+    return view('products', [
+        'products' => [
+            [
+                'id' => 1,
+                'title' => 'Sunset Dangles',
+                'price' => '$10',
+                'style' => 'dangles',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Ocean Hoops',
+                'price' => '$12',
+                'style' => 'hoops',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Pastel Studs',
+                'price' => '$8',
+                'style' => 'studs',
+            ],
+        ]
+    ]);
+});
+
+Route::get('/products/{id}', function($id) {
+    $products = [
+        [
+            'id' => 1,
+            'title' => 'Sunset Dangles',
+            'price' => '$10',
+            'style' => 'dangles',
+        ],
+        [
+            'id' => 2,
+            'title' => 'Ocean Hoops',
+            'price' => '$12',
+            'style' => 'hoops',
+        ],
+        [
+            'id' => 3,
+            'title' => 'Pastel Studs',
+            'price' => '$8',
+            'style' => 'studs',
+        ],
+    ];
+
+    $product = Arr::first($products, fn($product) => $product['id'] == $id);
+
+    return view('product', ['product' => $product]);
 });
 
 Route::get('/contact', function() {
